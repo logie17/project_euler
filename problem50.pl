ea8524666d5ea84bd55f1f712b63d93b513616ae
Logan Bell <loganbell@gmail.com>
2014-01-20 21:09:21 -0800

    Solved fool

--- problem50.pl
+++ problem50.pl
@@ -1,18 +1,19 @@
 #! /usr/bin/env perl
 use strict;
 use warnings;
+use List::Util qw(sum);
 
 sub find_primes_til {
   my ($cap) = @_;
-
+  warn "Finding primes";
   my @primes = ();
 
   @primes = (1) x $cap;
 
   $primes[0] = 0;
-  $primes[1] = 1;
+  $primes[1] = 0;
 
-  my @actual_prime_numbers = (1);
+  my @actual_prime_numbers = ();
 
   # Number theory thingy called Sieve of Eratosthenes
   for (my $x = 2; $x <= $cap; $x++) {
@@ -26,15 +27,35 @@ sub find_primes_til {
   return @actual_prime_numbers;
 }
 
-find_longest_consecutive_sum {
-  my @primes = @_;
+sub find_longest_consecutive_sum {
+  my $below = shift;
 
-  my $sum = $primes[0]; # prime the pump, get it!hahah
-  for(my $x = 1; $x < @primes; $x++) {
-    if ( $primes[$x] 
-    $sum += $primes[$x];       
+  my @primes = @_;
+  my %primes = map { $_ => 1 } @primes;
+
+  my $sets = {};
+
+  for (my $y = 0; $y <= scalar @primes; $y++) {
+    my @current_set = ();
+    for (my $x = $y; $x <= scalar @primes; $x++) {
+      push @current_set, $primes[$x];
+      my $sum = sum @current_set;
+
+      if ($sum > $below) {
+        last;
+      }
+
+      if ( $sum < $below && $primes{$sum} ) {
+        $sets->{scalar @current_set} = [$sum, \@current_set];
+      }
+    }
   }
+
+  my $top = (sort { $b <=> $a } keys %$sets)[0];
+  return $sets->{$top}[0];
 }
 
-my @prime_table = find_primes_til(100);
-use Data::Dumper; warn Dumper \@prime_table;
+my @prime_table = find_primes_til(1000000);
+my $longest = find_longest_consecutive_sum(1000000,@prime_table);
+# 997651
+warn $longest;
